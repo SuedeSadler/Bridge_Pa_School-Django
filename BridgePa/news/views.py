19160daffd890d5484cdeec1e8a2fb655ab86d5c
Suede Sadler <Suede.lang.sadler@gmail.com>
2022-12-11 18:50:57 +1300

    updates

--- BridgePa/news/views.py
+++ BridgePa/news/views.py
@@ -1,13 +1,26 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 # Create your views here.
-post = [
+posts = [
     {
-        
+        'Author': 'Suede',
+        'title': 'News Post 1',
+        'content': ' First news post content',
+        'date_posted': 'August 29, 2022 '
+    },
+    {
+        'Author': 'Philli',
+        'title': 'News Post 2',
+        'content': ' Second news post content',
+        'date_posted': 'August 25, 2022 '
     }
 ]
 def home(request):
-    return render(request, 'news/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'news/home.html', context)
 
 def about(request):
-    return render(request, 'news/about.html')
+    return render(request, 'news/about.html', {'title': 'About'})
